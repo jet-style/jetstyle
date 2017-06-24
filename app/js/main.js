ea0cac26e3d606a65359c8bf05d46f4cc25a1851
fatalnyishod <fatalnyishod@gmail.com>
2017-06-25 04:06:35 +0500

    commit

--- app/js/main.js
+++ app/js/main.js
@@ -1,19 +1,29 @@
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:1
-        },
-        1000:{
-            items:1
-        }
+
+// подключаем слайдер
+$( '.owl-carousel' ).owlCarousel( {
+
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+
+        0: {items: 1},
+        600: {items: 1},
+        1000: {items: 1}
+
     }
-})
+
+} );
 
 
+// стилизация селектов
+$( 'select' ).each( function() {
+
+    $( this ).siblings( 'p' ).text( $( this ).children('option:selected').text() );
+} );
+
+$( 'select' ).change( function() {
+
+    $( this ).siblings( 'p' ).text( $( this ).children('option:selected').text() );
+} );
 
